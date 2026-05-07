51ba072166d1a0def7a7f16cc7b2d8ff7c84dd58
halothekidd <halothekidd@gmail.com>
2026-05-07 13:28:25 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,42 @@
+let currentUser = null;
 
+/* REGISTER */
+async function register(username, password){
+    const res = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    });
+
+    return await res.json();
+}
+
+/* LOGIN */
+async function login(username, password){
+    const res = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    });
+
+    const data = await res.json();
+
+    if(data.success){
+        currentUser = username;
+        localStorage.setItem("user", username);
+    }
+
+    return data;
+}
+
+/* LOGOUT */
+function logout(){
+    currentUser = null;
+    localStorage.removeItem("user");
+}
+
+/* CHECK SESSION */
+function checkAuth(){
+    currentUser = localStorage.getItem("user");
+    return currentUser;
+}
