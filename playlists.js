bffb209c756078bc5bedf0fc3838f669959a9b21
halothekidd <halothekidd@gmail.com>
2026-05-07 13:29:11 -0400

    Update playlists.js

--- playlists.js
+++ playlists.js
@@ -1,1 +1,34 @@
+let playlists = {};
+let currentPlaylist = null;
 
+/* CREATE PLAYLIST */
+function createPlaylist(name){
+    if(!playlists[name]){
+        playlists[name] = [];
+    }
+    currentPlaylist = name;
+}
+
+/* ADD SONG TO PLAYLIST */
+function addToPlaylist(song){
+    if(!currentPlaylist) return;
+
+    playlists[currentPlaylist].push(song);
+}
+
+/* REMOVE SONG */
+function removeFromPlaylist(name, index){
+    playlists[name].splice(index, 1);
+}
+
+/* GET PLAYLIST */
+function getPlaylist(name){
+    return playlists[name] || [];
+}
+
+/* PLAY ENTIRE PLAYLIST */
+function playPlaylist(name){
+    if(!playlists[name] || playlists[name].length === 0) return;
+
+    playSong(playlists[name][0], playlists[name], 0);
+}
